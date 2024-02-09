c8096b3f1319248e22b67e98cf66147cdbdceeb9
Botas <143020046+GuilhermeCust74@users.noreply.github.com>
2024-02-09 12:11:14 -0300

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,16 @@
+// console.log("Hello Word"); 
+
+var x=7;
+var y=7;
+
+
+if (x == y ) {
+    console.log("Números conferem!")
+}else{
+    console.log("Números divergem")
+}
+
+/**
+ * DESAFIO
+ * Fazer aparecer a mensagem "Números conferem" no console
+ */
